c556e7f06b44d2179e743bd1e8ec92a4034f4fed
inker <anton.veselyev@gmail.com>
2019-03-28 15:35:52 +0000

    v0.0.6

---
+++ test/pump/index.js
@@ -0,0 +1,16 @@
+const fs = require('fs')
+const path = require('path')
+
+const { pump } = require('../../build')
+
+console.log(pump)
+
+async function main() {
+  const readStream = fs.createReadStream(path.resolve(`${__dirname}/data/file.txt`))
+  const writeStream = fs.createWriteStream(path.resolve(`${__dirname}/out/file.txt`))
+  console.log('pumping')
+  await pump(readStream, writeStream)
+  console.log('done')
+}
+
+main()
