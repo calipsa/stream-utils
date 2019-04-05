80fa5f2f4831387d160a07c1c1340cd3866b9c76
inker <anton.veselyev@gmail.com>
2019-04-05 14:01:48 +0100

    update tests

--- test/conversions.test.js
+++ test/conversions.test.js
@@ -5,10 +5,14 @@ const {
 } = require('../build')
 
 const ENCODING = 'utf8'
-const INPUT_TEXT = 'Random text to be used for the test'
 const NUM_SPLIT_STREAMS = 10
 
-const BUFFER = Buffer.from(INPUT_TEXT, ENCODING)
+const texts = {
+  random: 'Random text to be used for the test',
+  empty: '',
+  unicode: 'üñíçöďé ťęxţ',
+  long: 'abc'.repeat(100000),
+}
 
 const allItemsAreEqual = (arr) =>
   arr.every(item => item === arr[0])
@@ -18,25 +22,31 @@ describe('Conversions', () => {
     jest.setTimeout(120000)
   })
 
-  it('Should convert correctly', async () => {
-    const stream = bufferToStream(BUFFER)
-    const convertedBuffer = await streamToBuffer(stream)
-    const outputText = convertedBuffer.toString(ENCODING)
-    expect(outputText).toBe(INPUT_TEXT)
-  })
+  for (const [type, text] of Object.entries(texts)) {
+    console.log('text:', type)
 
-  it('Should convert & split correctly', async () => {
-    const stream = bufferToStream(BUFFER)
-    const streams = splitStream(stream, NUM_SPLIT_STREAMS)
-    const promises = streams.map(streamToBuffer)
-    const convertedBuffers = await Promise.all(promises)
-    const outputTexts = convertedBuffers.map(item => item.toString(ENCODING))
-    
-    expect(outputTexts.length).toBe(NUM_SPLIT_STREAMS)
-    const firstOutputText = outputTexts[0]
-    for (let i = 0; i < NUM_SPLIT_STREAMS; ++i) {
-      expect(outputTexts[i]).toBe(firstOutputText)
-    }
-    expect(firstOutputText).toBe(INPUT_TEXT)
-  })
+    it(`Should convert correctly (${type})`, async () => {
+      const buffer = Buffer.from(text, ENCODING)
+      const stream = bufferToStream(buffer)
+      const convertedBuffer = await streamToBuffer(stream)
+      const outputText = convertedBuffer.toString(ENCODING)
+      expect(outputText).toBe(text)
+    })
+  
+    it(`Should convert & split correctly (${type})`, async () => {
+      const buffer = Buffer.from(text, ENCODING)
+      const stream = bufferToStream(buffer)
+      const streams = splitStream(stream, NUM_SPLIT_STREAMS)
+      const promises = streams.map(streamToBuffer)
+      const convertedBuffers = await Promise.all(promises)
+      const outputTexts = convertedBuffers.map(item => item.toString(ENCODING))
+      
+      expect(outputTexts.length).toBe(NUM_SPLIT_STREAMS)
+      const firstOutputText = outputTexts[0]
+      for (let i = 0; i < NUM_SPLIT_STREAMS; ++i) {
+        expect(outputTexts[i]).toBe(firstOutputText)
+      }
+      expect(firstOutputText).toBe(text)
+    })
+  }
 })
