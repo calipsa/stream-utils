06f5c11f7832dde40cc70b27d4a385946edb2a54
inker <anton.veselyev@gmail.com>
2020-05-18 14:11:51 +0100

    fix StreamWithFrames

--- src/StreamWithFrames.ts
+++ src/StreamWithFrames.ts
@@ -1,6 +1,19 @@
 import { Writable } from 'stream'
 
-const JPG_DELIMITER = Buffer.from('FFD8FF', 'hex')
+const JPG_START = Buffer.from('FFD8FF', 'hex')
+const JPG_END = Buffer.from('FFD9', 'hex')
+
+function getEnd(currentBuffer: Buffer, start: number) {
+  // const originalEnd = currentBuffer.indexOf(JPG_DELIMITER, start + JPG_DELIMITER.length)
+  // if (originalEnd > -1) {
+  //   return originalEnd
+  // }
+  const alternativeEnd = currentBuffer.indexOf(JPG_END, start + JPG_START.length)
+  return alternativeEnd > -1
+    ? alternativeEnd + JPG_END.length
+    // : originalEnd
+    : alternativeEnd
+}
 
 class StreamWithFrames extends Writable {
   readonly #rate: number
@@ -21,12 +34,12 @@ class StreamWithFrames extends Writable {
     this.#currentBuffer = Buffer.concat([this.#currentBuffer, data])
     // eslint-disable-next-line no-constant-condition
     while (true) {
-      const start = this.#currentBuffer.indexOf(JPG_DELIMITER)
+      const start = this.#currentBuffer.indexOf(JPG_START)
       if (start < 0) {
         // there's no frame data at all
         break
       }
-      const end = this.#currentBuffer.indexOf(JPG_DELIMITER, start + JPG_DELIMITER.length)
+      const end = getEnd(this.#currentBuffer, start)
       if (end < 0) {
         // we haven't got the whole frame yet
         break
